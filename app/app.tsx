16743beae891b49c8d5ccccada38da1e94a58d08
Azrilardian05 <azril@itsavirus.com>
2025-04-28 15:06:44 +0800

    fix(app): update loading component opacity for improved visibility

--- app/app.tsx
+++ app/app.tsx
@@ -11,7 +11,7 @@ import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 const Header = dynamic(() => import('@/components/header'), {
   ssr: false,
   loading: () => (
-    <div className='bg-grey-lightest sticky top-0 z-10 h-16 animate-pulse border-b px-4' />
+    <div className='bg-grey-lightest sticky top-0 z-10 h-16 animate-pulse border-b px-4 opacity-100' />
   )
 })
 
