a78db01ad4c44e9157b3d43dbce6bc5b1598619a
Azrilardian05 <azril@itsavirus.com>
2025-04-28 15:56:48 +0800

    feat(button): change file naming and add customizable props and loading state

--- src/components/button-group/button-group.types.ts
+++ src/components/button-group/button-group.types.ts
@@ -1,6 +1,6 @@
 import type { ComponentProps } from 'react'
 
-import type { ControlledButtonProps } from '../button/types'
+import type { ControlledButtonProps } from '../button/button.types'
 
 export type ButtonGroupProps = ComponentProps<'div'> & {
   buttons: [ControlledButtonProps, ControlledButtonProps]
