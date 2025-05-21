1f8d4f8b3a27f0f67d572a502403063f2591c805
Azrilardian <irfan05ariell@gmail.com>
2025-05-21 17:44:15 +0800

    fix(modal): remove horizontal padding

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -34,7 +34,7 @@ function AlertDialogOverlay({
       {...props}
     >
       <div className='absolute inset-0 z-50'>
-        <Image src={modalOverlay} alt='modal-overlay' fill />
+        <Image src={modalOverlay} alt='' fill />
       </div>
     </AlertDialogPrimitive.Overlay>
   )
@@ -50,7 +50,7 @@ function AlertDialogContent({
       <AlertDialogPrimitive.Content
         data-slot='alert-dialog-content'
         className={cn(
-          'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-8 rounded-lg border p-8 shadow-lg duration-200 sm:max-w-lg',
+          'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-8 rounded-lg border px-10 py-8 shadow-lg duration-200 sm:max-w-lg',
           className
         )}
         {...props}
