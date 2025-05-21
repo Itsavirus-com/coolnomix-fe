ea8ad2b6e75fe9740691eb32657a3697a8f28b77
Azrilardian <irfan05ariell@gmail.com>
2025-05-21 22:06:29 +0800

    feat(components): add and adjust shadcn card component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: ComponentProps<'div'>) {
     <div
       data-slot='card'
       className={cn(
-        'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm',
+        'bg-card text-card-foreground border-color-50 grid grid-rows-[auto_1fr] rounded-xl border px-3 py-5',
         className
       )}
       {...props}
@@ -20,7 +20,7 @@ function CardHeader({ className, ...props }: ComponentProps<'div'>) {
     <div
       data-slot='card-header'
       className={cn(
-        '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
+        '@container/card-header mb-2.5 grid auto-rows-min grid-rows-[auto_auto] items-start px-2 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
         className
       )}
       {...props}
@@ -59,7 +59,7 @@ function CardAction({ className, ...props }: ComponentProps<'div'>) {
 }
 
 function CardContent({ className, ...props }: ComponentProps<'div'>) {
-  return <div data-slot='card-content' className={cn('px-6', className)} {...props} />
+  return <div data-slot='card-content' className={className} {...props} />
 }
 
 function CardFooter({ className, ...props }: ComponentProps<'div'>) {
