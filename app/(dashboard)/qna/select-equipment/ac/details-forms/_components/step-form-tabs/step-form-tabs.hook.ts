51b7387a7892fd61372525a39f11afc8d0840a11
Azrilardian <irfan05ariell@gmail.com>
2025-06-01 19:00:43 +0800

    feat: add URL params management and optimize tab handling

--- app/(dashboard)/qna/select-equipment/ac/details-forms/_components/step-form-tabs/step-form-tabs.hook.ts
+++ app/(dashboard)/qna/select-equipment/ac/details-forms/_components/step-form-tabs/step-form-tabs.hook.ts
@@ -1,34 +1,18 @@
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 import { qnaAcDetailsReviewPath } from '@/config/paths'
+import { useStepParams } from '@/hooks/url-params.hook'
 
 const steps = ['upload-form', 'details-form', 'details-form-ac']
 
 export const useStepFormTabs = () => {
   const router = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
 
-  const step = searchParams.get('step') || 'upload-form'
-
-  const [currentStep, setCurrentStep] = useState(step)
-
-  const handleContinue = (step: string) => {
-    setCurrentStep(step)
-
-    const params = new URLSearchParams(searchParams)
-    params.set('step', step)
-    window.history.pushState({}, '', pathname + '?' + params.toString())
-  }
+  const { currentValue, updateParam } = useStepParams('upload-form')
 
   const handleReview = () => {
     router.push(qnaAcDetailsReviewPath({ type: 'details-forms' }))
   }
 
-  useEffect(() => {
-    setCurrentStep(step)
-  }, [step])
-
-  return { steps, currentStep, handleContinue, handleReview }
+  return { steps, currentStep: currentValue, handleContinue: updateParam, handleReview }
 }
