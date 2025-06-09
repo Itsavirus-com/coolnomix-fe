57e9c52cada34fbc42dd13fd91aa2a5c00ed21c1
Azrilardian <irfan05ariell@gmail.com>
2025-06-09 11:42:53 +0800

    feat(select): add UncontrolledSelect component and update select types

---
+++ src/components/forms/select/UncontrolledSelect.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import React, { FC } from 'react'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import { cn } from '@/libs/utils'
+
+import { UncontrolledSelectProps } from './select.types'
+
+const UncontrolledSelect: FC<UncontrolledSelectProps> = (props) => {
+  const { items, placeholder, value, onValueChange, className, disabled } = props
+
+  return (
+    <Select value={value} onValueChange={onValueChange} disabled={disabled}>
+      <SelectTrigger className={cn('h-8', className)}>
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        {items.map((item) => (
+          <SelectItem key={item.value} value={item.value}>
+            {item.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  )
+}
+
+export default UncontrolledSelect
