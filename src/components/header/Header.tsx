f55216620ae892fa063e5f931272e4fbf1533df4
Azrilardian <irfan05ariell@gmail.com>
2025-06-09 11:48:30 +0800

    feat(header): enhance header component with button integration and improved layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,6 +6,8 @@ import { useSnapshot } from 'valtio'
 
 import { appStateStore } from '@/stores/app-state-store'
 
+import Button from '../button/Button'
+
 const Icon = dynamic(() => import('../icon/Icon'), { ssr: false })
 const Text = dynamic(() => import('../text/Text'), { ssr: false })
 
@@ -13,16 +15,24 @@ const Header = () => {
   const { icon, title } = useSnapshot(appStateStore).state.pageHeader
 
   return (
-    <header className='bg-background sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b px-4'>
+    <header className='bg-background sticky top-0 z-10 flex h-16 shrink-0 items-center justify-between gap-2 border-b px-8'>
       <div className='flex items-center gap-1.5'>
         <Icon icon={icon} size={18} />
         <Text weight='semibold' tag='span'>
           {title}
         </Text>
       </div>
-      <div className='flex items-center gap-[18px]'>
-        <Icon icon='icon-bell-dot' size={20} />
-        <Icon icon='icon-circle-help' size={20} />
+      <div className='flex items-center gap-1'>
+        <Button
+          variant='ghost'
+          size='icon'
+          label={<Icon icon='icon-bell-dot' className='text-black' size={20} />}
+        />
+        <Button
+          variant='ghost'
+          size='icon'
+          label={<Icon icon='icon-circle-help' className='text-black' size={20} />}
+        />
       </div>
     </header>
   )
